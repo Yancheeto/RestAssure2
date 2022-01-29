02226dc7b2102e91e66d5a20f54ff887789cd865
Yancheeto <yanni.stoyanova@gmail.com>
2022-01-29 14:47:17 -0500

    TestBase

--- src/test/java/com/cybertek/tests/day04_ords_path/PRDSGetRequestsTest.java
+++ src/test/java/com/cybertek/tests/day04_ords_path/PRDSGetRequestsTest.java
@@ -1,14 +1,20 @@
 package com.cybertek.tests.day04_ords_path;
 
 import com.cybertek.utilities.ConfigurationReader;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
 
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
 public class PRDSGetRequestsTest {
 
-    @BeforeAll
+    @BeforeAll  //runs ones before all tests
     public static void setUp(){
     baseURI = ConfigurationReader.getProperty("ords.url");
 
@@ -21,11 +27,81 @@ public class PRDSGetRequestsTest {
      * Content type should be "application/json"
      * And body should contain "Europe"
      */
+    @DisplayName("Get Request to /ords/hr/regions")
     @Test
     public void getAllRegionsTest(){
+        Response response = given().accept(ContentType.JSON)
+                .when().get("/regions");
+        System.out.println("status code = " + response.statusCode());
+        assertEquals(200, response.statusCode());
 
+        System.out.println("content type = " + response.contentType());
+        assertEquals("application/json", response.contentType());
+
+        response.prettyPrint();
+
+        assertTrue(response.asString().contains("Europe"));
+
+    }
+
+    /**
+     * Given accept type is json
+     * And Path param value is 1
+     * When user send get request to /ords/hr/regions/{region_id}
+     * Status code should be 200
+     * Content type should be "application/json"
+     * And body should contain "Europe"
+     */
+    @DisplayName("Get Request to /ords/hr/regions/1")
+    @Test
+    public void getSingleRegionsWithParam(){
+    int regionId = 1;
+    Response response = given().accept(ContentType.JSON)
+            .and().pathParam("id", regionId)
+            .when().get("/regions/{id}");
+            //.when().get("/regions/" + regionId);        --> also works this way
+
+        System.out.println("status code = " + response.statusCode());
+        assertEquals(200, response.statusCode());
+
+        System.out.println("content type = " + response.contentType());
+        assertEquals("application/json", response.contentType());
+
+        response.prettyPrint();
+
+        assertTrue(response.asString().contains("Europe"));
+
+    }
+
+    /**
+     * Given accept type is json
+     * And query param q={"region_name": "Americas"}
+     * When user send get request to /ords/hr/regions
+     * Status code should be 200
+     * Content type should be "application/json"
+     * And region name should be "Americas"
+     * And region id should be 1
+     */
+    @DisplayName("Get Request or /ords/hr/regions with query param")
+    @Test
+    public void getRegionWithQueryParam() {
+    Response response = given().accept(ContentType.JSON)
+            .and().queryParam("q", "{\"region_name\": \"Americas\"}" )
+            .and().when().get("regions");
+
+        System.out.println("status code = " + response.statusCode());
+        assertEquals(200, response.statusCode());
+
+        System.out.println("content type = " + response.contentType());
+        assertEquals("application/json", response.contentType());
+
+        response.prettyPrint();
+
+        assertTrue(response.asString().contains("Americas"));
+        assertTrue(response.asString().contains("2"));
 
 
     }
+
 
 }
