fc0e76207377e016909ce64e9c092b8904170336
Yancheeto <yanni.stoyanova@gmail.com>
2022-01-30 12:45:21 -0500

    Update day 5/jsonPath

--- src/test/java/com/cybertek/tests/day05_jsonPath/ORDSGetEmployeesPathTest.java
+++ src/test/java/com/cybertek/tests/day05_jsonPath/ORDSGetEmployeesPathTest.java
@@ -7,8 +7,9 @@ import com.cybertek.tests.ORDSTestBase;
 import org.junit.jupiter.api.Test;
 import io.restassured.response.Response;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-
+import static org.junit.jupiter.api.Assertions.*;
 public class ORDSGetEmployeesPathTest extends ORDSTestBase {
 
     @Test
@@ -32,9 +33,23 @@ public class ORDSGetEmployeesPathTest extends ORDSTestBase {
         System.out.println("first emp last name = " +response.path("items[0].last_name"));
         System.out.println("first emp email = " +response.path("items[0].email"));
 
+        //you want to email all IT_PROG, save all email into list of String
 
+        List<String> allEmails = response.path("items.email");
+        System.out.println("all emails = " + allEmails);
+        System.out.println("count of emails  = " + allEmails.size());
 
+        //you want to text all IT_PROG and need their phones, save their phone is the List of String
+
+        List<String> allPhones = response.path("items.phone_number");
+        System.out.println("all phone numbers count = " + allPhones.size());
+        System.out.println(" all Phones = " + allPhones);
+
+    //verify that 590.423.4568 is among phone numbers
+        assertTrue(allPhones.contains("590.423.4568"));
     }
+
+
 
 
 }
